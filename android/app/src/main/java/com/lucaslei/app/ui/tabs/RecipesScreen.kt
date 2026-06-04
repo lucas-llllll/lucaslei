6cce503bfe7d9b2351ce0ace6822ae9c6cca3f95
lucas-llllll <lei346658476@gmail.com>
2026-06-04 12:20:47 +0800

    fix experimental API in android/app/src/main/java/com/lucaslei/app/ui/tabs/RecipesScreen.kt

--- android/app/src/main/java/com/lucaslei/app/ui/tabs/RecipesScreen.kt
+++ android/app/src/main/java/com/lucaslei/app/ui/tabs/RecipesScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.lucaslei.app.MainViewModel
 import com.lucaslei.app.Recipe
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipesScreen(vm: MainViewModel) {
     val recipes by vm.recipes.collectAsState()
