d326840d11fe138cc62a29876c33a9c3f6755b05
lucas-llllll <lei346658476@gmail.com>
2026-06-04 13:06:42 +0800

    fix android/app/src/main/java/com/lucaslei/app/LucasleiApp.kt

--- android/app/src/main/java/com/lucaslei/app/LucasleiApp.kt
+++ android/app/src/main/java/com/lucaslei/app/LucasleiApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lucaslei.app.ui.LucasleiTheme
