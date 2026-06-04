ba1aaaeb71ad489059e1ac5f3d145432f487dbc3
lucas-llllll <lei346658476@gmail.com>
2026-06-04 10:23:10 +0800

    restore android/app/src/main/java/com/lucaslei/app/MainActivity.kt

---
+++ android/app/src/main/java/com/lucaslei/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lucaslei.app
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { LucasleiApp() }
+    }
+}
